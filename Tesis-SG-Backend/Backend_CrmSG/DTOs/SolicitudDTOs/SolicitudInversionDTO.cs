e2e23b714da772829fc15fdccc3a160cfe3981a9
Kevin Rosero <kevinrosero199772@gmail.com>
2025-05-10 17:37:02 -0500

    Cambios id vistas y update completado

--- Tesis-SG-Backend/Backend_CrmSG/DTOs/SolicitudDTOs/SolicitudInversionDTO.cs
+++ Tesis-SG-Backend/Backend_CrmSG/DTOs/SolicitudDTOs/SolicitudInversionDTO.cs
@@ -4,6 +4,24 @@ namespace Backend_CrmSG.DTOs.SolicitudDTOs
 {
     public class SolicitudInversionDTO
     {
+        public int IdSolicitudInversion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public string? JSONDocument { get; set; }
+
+        public int? IdProspecto { get; set; }
+        public string? NombreCompletoProspecto { get; set; }
+
+        public int? IdCliente { get; set; }
+        public string? NombreCompletoCliente { get; set; }
+
+        public int? IdUsuarioPropietario { get; set; }
+        public string? NombrePropietario { get; set; }
+
+        public int? IdUsuarioModificacion { get; set; }
+        public string? NombreModificacion { get; set; }
+
+
         public IdentificacionDTO? Identificacion { get; set; }
         public ProyeccionDTO? Proyeccion { get; set; }
         public DatosGeneralesDTO? DatosGenerales { get; set; }
